c06eec63b99c5ac48a7b9c4b2455ccc77bc1331d
yamachan0625 <napoli.080625@gmail.com>
2021-04-25 17:08:27 +0900

    関数の移動

--- 1-example.js
+++ 1-example.js
@@ -28,6 +28,30 @@ function statement(invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
+
+  function amountFor(aPerformance /** 名前から型がわかるようにする */) {
+    let result = 0; // 関数の戻り値を示す面数名は常にresultにすると役割が明確になる
+    switch (aPerformance.play.type) {
+      case 'tragedy': {
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      }
+      case 'comedy': {
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      }
+      default:
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -75,30 +99,6 @@ function renderPlainText(data, plays) {
     if ('comedy' === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
 
-    return result;
-  }
-
-  function amountFor(aPerformance /** 名前から型がわかるようにする */) {
-    let result = 0; // 関数の戻り値を示す面数名は常にresultにすると役割が明確になる
-    switch (aPerformance.play.type) {
-      case 'tragedy': {
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      }
-      case 'comedy': {
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      }
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
     return result;
   }
 }
