ab2e62d61c9b083f980ff1fdf78becab0f22c520
yamachan0625 <napoli.080625@gmail.com>
2021-04-25 15:24:31 +0900

    変数のインライン化

--- 1-example.js
+++ 1-example.js
@@ -54,15 +54,15 @@ function statement(invoice, plays) {
   }
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // ボリュームの得点のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 喜劇の時は10人につきさらにポイントを加算
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
     //注文の内訳を表示
-    result += ` ${play.name}:${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}:${format(thisAmount / 100)} (${
       perf.audience
     } seats) \n`;
     totalAmount += thisAmount;
